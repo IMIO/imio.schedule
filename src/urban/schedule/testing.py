809ce8c8ae4c5c0830a7b6002be29d77bc75ef81
Simon Delcourt <delcourt.simon@gmail.com>
2016-01-14 15:21:59 +0100

    Add dependencies tests.

--- src/urban/schedule/testing.py
+++ src/urban/schedule/testing.py
@@ -1,46 +1,70 @@
 # -*- coding: utf-8 -*-
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import login
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from plone.testing import z2
+
+import transaction
 
 import urban.schedule
 
 
-class UrbanScheduleLayer(PloneSandboxLayer):
+class NakedPloneLayer(PloneSandboxLayer):
 
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
-        self.loadZCML(package=urban.schedule)
+        """Set up Zope."""
+        # Load ZCML
+        self.loadZCML(package=urban.schedule,
+                      name='testing.zcml')
+        z2.installProduct(app, 'urban.schedule')
+
+    def tearDownZope(self, app):
+        """Tear down Zope."""
+        z2.uninstallProduct(app, 'urban.schedule')
+
+NAKED_PLONE_FIXTURE = NakedPloneLayer(
+    name='NAKED_PLONE_FIXTURE'
+)
+
+NAKED_PLONE_INTEGRATION = IntegrationTesting(
+    bases=(NAKED_PLONE_FIXTURE,),
+    name='NAKED_PLONE_INTEGRATION'
+)
+
+
+class UrbanScheduleLayer(NakedPloneLayer):
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'urban.schedule:default')
 
+        # Login and create some test content
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
 
-URBAN_SCHEDULE_FIXTURE = UrbanScheduleLayer()
+        # Commit so that the test browser sees these objects
+        transaction.commit()
 
 
-URBAN_SCHEDULE_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(URBAN_SCHEDULE_FIXTURE,),
-    name='UrbanScheduleLayer:IntegrationTesting'
+TEST_INSTALL_FIXTURE = UrbanScheduleLayer(
+    name='TEST_INSTALL_FIXTURE'
+)
+
+TEST_INSTALL_INTEGRATION = IntegrationTesting(
+    bases=(TEST_INSTALL_FIXTURE,),
+    name='TEST_INSTALL_INTEGRATION'
 )
 
 
-URBAN_SCHEDULE_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(URBAN_SCHEDULE_FIXTURE,),
-    name='UrbanScheduleLayer:FunctionalTesting'
-)
-
-
-URBAN_SCHEDULE_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        URBAN_SCHEDULE_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE
-    ),
-    name='UrbanScheduleLayer:AcceptanceTesting'
+TEST_INSTALL_FUNCTIONAL = FunctionalTesting(
+    bases=(TEST_INSTALL_FIXTURE,),
+    name='TEST_INSTALL_FUNCTIONAL'
 )
--- src/urban/schedule/tests/test_setup.py
+++ src/urban/schedule/tests/test_setup.py
@@ -1,15 +1,43 @@
 # -*- coding: utf-8 -*-
-"""Setup tests for this package."""
-from urban.schedule.testing import URBAN_SCHEDULE_INTEGRATION_TESTING  # noqa
+
+from urban.schedule.testing import NAKED_PLONE_INTEGRATION
+from urban.schedule.testing import TEST_INSTALL_INTEGRATION  # noqa
+
 from plone import api
+from plone.app.testing import applyProfile
 
 import unittest
+
+
+class TestInstallDependencies(unittest.TestCase):
+
+    layer = NAKED_PLONE_INTEGRATION
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_dexterity_is_dependency_of_urban_schedule(self):
+        """
+        dexterity should be installed when we install urban.schedule
+        """
+        self.assertTrue(not self.installer.isProductInstalled('plone.app.dexterity'))
+        applyProfile(self.portal, 'urban.schedule:default')
+        self.assertTrue(self.installer.isProductInstalled('plone.app.dexterity'))
+
+    def test_z3cformdatagridfield_is_dependency_of_urban_schedule(self):
+        """
+        z3cform.datagridfield should be installed when we install urban.schedule
+        """
+        self.assertTrue(not self.installer.isProductInstalled('collective.z3cform.datagridfield'))
+        applyProfile(self.portal, 'urban.schedule:default')
+        self.assertTrue(self.installer.isProductInstalled('collective.z3cform.datagridfield'))
 
 
 class TestSetup(unittest.TestCase):
     """Test that urban.schedule is properly installed."""
 
-    layer = URBAN_SCHEDULE_INTEGRATION_TESTING
+    layer = TEST_INSTALL_INTEGRATION
 
     def setUp(self):
         """Custom shared utility setup for tests."""
@@ -18,20 +46,18 @@ class TestSetup(unittest.TestCase):
 
     def test_product_installed(self):
         """Test if urban.schedule is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'urban.schedule'))
+        self.assertTrue(self.installer.isProductInstalled('urban.schedule'))
 
     def test_browserlayer(self):
         """Test that IUrbanScheduleLayer is registered."""
-        from urban.schedule.interfaces import (
-            IUrbanScheduleLayer)
+        from urban.schedule.interfaces import IUrbanScheduleLayer
         from plone.browserlayer import utils
         self.assertIn(IUrbanScheduleLayer, utils.registered_layers())
 
 
 class TestUninstall(unittest.TestCase):
 
-    layer = URBAN_SCHEDULE_INTEGRATION_TESTING
+    layer = TEST_INSTALL_INTEGRATION
 
     def setUp(self):
         self.portal = self.layer['portal']
