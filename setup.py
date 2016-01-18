54c09473d7dbc4b74483529c4b8ee307ef154065
Simon Delcourt <delcourt.simon@gmail.com>
2016-01-18 14:57:26 +0100

    Remove plone.app.contenttypes from dependencies.

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     extras_require={
         'test': [
             'plone.app.testing',
-            'plone.app.contenttypes',
             'plone.app.robotframework[debug]',
         ],
     },
