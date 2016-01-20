acc81f42681aaf5833529e148300ad97810b1d19
Simon Delcourt <delcourt.simon@gmail.com>
2016-01-20 16:34:56 +0100

    Test default voc registration for content_types field.

--- src/urban/schedule/tests/test_vocabulary.py
+++ src/urban/schedule/tests/test_vocabulary.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
+
 from urban.schedule.testing import ExampleScheduleIntegrationTestCase
 
+from zope.component import queryAdapter
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
 
@@ -11,12 +14,36 @@ class TestVocabularies(ExampleScheduleIntegrationTestCase):
     Test field vocabularies registration and values.
     """
 
+    def _get_fti(self, portal_type):
+        """
+        Helper method to return the fti of a content type.
+        """
+        portal_types = api.portal.get_tool('portal_types')
+        fti = portal_types.getTypeInfo(portal_type)
+        return fti
+
     def test_content_types_vocabulary_factory_registration(self):
         """
         Content types voc factory should be registered as a named utility.
         """
         factory_name = 'urban.schedule.content_types'
         self.assertTrue(queryUtility(IVocabularyFactory, factory_name))
+
+    def test_content_types_default_vocabulary_registration(self):
+        """
+        Voc values should be registered as a named adapter on the task
+        config fti and name should be the fti portal_type.
+        """
+        from urban.schedule.interfaces import IContentTypesVocabulary
+
+        portal_type = self.taskconfig_1.portal_type
+
+        voc_adapter = queryAdapter(
+            self._get_fti(portal_type),
+            IContentTypesVocabulary,
+            portal_type
+        )
+        self.assertTrue(voc_adapter)
 
     def test_content_types_vocabulary_values(self):
         """
