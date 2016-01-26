740f0bda93fe570e32d8894fceef9dc54cd86f1a
Simon Delcourt <delcourt.simon@gmail.com>
2016-01-26 13:46:31 +0100

    Test utils.py methods.

---
+++ src/urban/schedule/tests/test_utils.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from Products.ATContentTypes.interfaces import IATFolder
+
+from urban.schedule.testing import ExampleScheduleIntegrationTestCase
+
+
+class TestUtils(ExampleScheduleIntegrationTestCase):
+    """
+    Thes all methods of utils.py module.
+    """
+
+    def test_get_task_configs(self):
+        """
+        Test the method get_task_configs
+
+        """
+        from urban.schedule.utils import get_task_configs
+
+        folder = self.portal.config
+        expected_taskconfigs = [self.test_taskconfig]
+        task_configs = get_task_configs(folder)
+        self.assertEqual(len(task_configs), len(expected_taskconfigs))
+        self.assertEqual(task_configs[0].UID(), expected_taskconfigs[0].UID())
+
+    def test_tuple_to_interface(self):
+        """
+        Should turn a tuple ('interface.module.path', 'Interface') into
+        Interface class.
+        """
+        from urban.schedule.utils import tuple_to_interface
+
+        expected_interface = IATFolder
+        interface_tuple = ('Products.ATContentTypes.interfaces.folder', 'IATFolder')
+        interface = tuple_to_interface(interface_tuple)
+        self.assertEqual(interface, expected_interface)
+
+    def test_interface_to_tuple(self):
+        """
+        Should turn an Interface class into a tuple:
+        ('interface.module.path', 'Interface')
+        """
+        from urban.schedule.utils import interface_to_tuple
+
+        expected_tuple = ('Products.ATContentTypes.interfaces.folder', 'IATFolder')
+        interface_tuple = interface_to_tuple(IATFolder)
+        self.assertEqual(interface_tuple, expected_tuple)
---
+++ src/urban/schedule/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from urban.schedule.interfaces import IToTaskConfig
+
+from zope.component import getAdapters
+
+import importlib
+
+
+def get_task_configs(task_container):
+    """
+    Return all the task configs to check for the given context
+    providing ITaskContainer.
+    """
+    config_adapters = getAdapters((task_container,), IToTaskConfig)
+    task_configs = [adapter.task_config for name, adapter in config_adapters]
+
+    return task_configs
+
+
+def tuple_to_interface(interface_tuple):
+    """
+    Turn a tuple of strings:
+    ('interface.module.path', 'Interface')
+    into an Interface object.
+    """
+    module_path, interface_name = interface_tuple
+    interface_module = importlib.import_module(module_path)
+    interface_class = getattr(interface_module, interface_name)
+
+    return interface_class
+
+
+def interface_to_tuple(interface):
+    """
+    Turn an Interface class into a tuple:
+    ('interface.module.path', 'Interface')
+    """
+    return (interface.__module__, interface.__name__)
