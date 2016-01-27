85210c8a9d39e143c0d0e178b620b68288e6ad79
Simon Delcourt <delcourt.simon@gmail.com>
2016-01-27 15:48:34 +0100

    Adapt tests.

--- src/urban/schedule/tests/test_vocabulary.py
+++ src/urban/schedule/tests/test_vocabulary.py
@@ -67,22 +67,6 @@ class TestVocabularies(ExampleScheduleIntegrationTestCase):
         factory_name = 'urban.schedule.start_conditions'
         self.assertTrue(queryUtility(IVocabularyFactory, factory_name))
 
-    def test_start_conditions_default_vocabulary_registration(self):
-        """
-        Voc values should be registered as a named adapter on the task
-        config fti and name should be the fti portal_type.
-        """
-        from urban.schedule.interfaces import IStartConditionsVocabulary
-
-        portal_type = self.test_taskconfig.portal_type
-
-        voc_adapter = queryAdapter(
-            self._get_fti(portal_type),
-            IStartConditionsVocabulary,
-            portal_type
-        )
-        self.assertTrue(voc_adapter)
-
     def test_start_conditions_vocabulary_values(self):
         """
         Test some start_conditions values.
@@ -99,22 +83,6 @@ class TestVocabularies(ExampleScheduleIntegrationTestCase):
         """
         factory_name = 'urban.schedule.end_conditions'
         self.assertTrue(queryUtility(IVocabularyFactory, factory_name))
-
-    def test_end_conditions_default_vocabulary_registration(self):
-        """
-        Voc values should be registered as a named adapter on the task
-        config fti and name should be the fti portal_type.
-        """
-        from urban.schedule.interfaces import IEndConditionsVocabulary
-
-        portal_type = self.test_taskconfig.portal_type
-
-        voc_adapter = queryAdapter(
-            self._get_fti(portal_type),
-            IEndConditionsVocabulary,
-            portal_type
-        )
-        self.assertTrue(voc_adapter)
 
     def test_end_conditions_vocabulary_values(self):
         """
