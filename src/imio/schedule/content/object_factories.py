7e2ea8a4391f097306f706e1a4cfab6aa344cf97
Simon Delcourt <delcourt.simon@gmail.com>
2016-05-18 08:30:58 +0200

    Factorize code for condition objects.

--- src/imio/schedule/content/object_factories.py
+++ src/imio/schedule/content/object_factories.py
@@ -12,102 +12,84 @@ from imio.schedule.content.task_config import IMacroStartConditionSchema
 from imio.schedule.content.task_config import IMacroEndConditionSchema
 
 
-class CreationConditionObject(object):
-    implements(ICreationConditionSchema)
-
-    condition = FieldProperty(ICreationConditionSchema['condition'])
-    operator = FieldProperty(ICreationConditionSchema['operator'])
+class BaseConditionObject(object):
+    """
+    Base class for condition objects.
+    """
 
     __name__ = ''
     __parent__ = None
 
+    def __init__(self, condition=None, operator=None):
+        if condition is not None:
+            self.__dict__['condition'] = condition
+        if operator is not None:
+            self.__dict__['operator'] = operator
+
     def getId(self):
         return self.__name__ or ''
+
+
+class CreationConditionObject(BaseConditionObject):
+    implements(ICreationConditionSchema)
+
+    condition = FieldProperty(ICreationConditionSchema['condition'])
+    operator = FieldProperty(ICreationConditionSchema['operator'])
 
 
 class CreationConditionAdapter(FactoryAdapter):
     factory = CreationConditionObject
 
 
-class StartConditionObject(object):
+class StartConditionObject(BaseConditionObject):
     implements(IStartConditionSchema)
 
     condition = FieldProperty(IStartConditionSchema['condition'])
     operator = FieldProperty(IStartConditionSchema['operator'])
-
-    __name__ = ''
-    __parent__ = None
-
-    def getId(self):
-        return self.__name__ or ''
 
 
 class StartConditionAdapter(FactoryAdapter):
     factory = StartConditionObject
 
 
-class EndConditionObject(object):
+class EndConditionObject(BaseConditionObject):
     implements(IEndConditionSchema)
 
     condition = FieldProperty(IEndConditionSchema['condition'])
     operator = FieldProperty(IEndConditionSchema['operator'])
-
-    __name__ = ''
-    __parent__ = None
-
-    def getId(self):
-        return self.__name__ or ''
 
 
 class EndConditionAdapter(FactoryAdapter):
     factory = EndConditionObject
 
 
-class MacroCreationConditionObject(object):
+class MacroCreationConditionObject(BaseConditionObject):
     implements(IMacroCreationConditionSchema)
 
     condition = FieldProperty(IMacroCreationConditionSchema['condition'])
     operator = FieldProperty(IMacroCreationConditionSchema['operator'])
-
-    __name__ = ''
-    __parent__ = None
-
-    def getId(self):
-        return self.__name__ or ''
 
 
 class MacroCreationConditionAdapter(FactoryAdapter):
     factory = MacroCreationConditionObject
 
 
-class MacroStartConditionObject(object):
+class MacroStartConditionObject(BaseConditionObject):
     implements(IMacroStartConditionSchema)
 
     condition = FieldProperty(IMacroStartConditionSchema['condition'])
     operator = FieldProperty(IMacroStartConditionSchema['operator'])
-
-    __name__ = ''
-    __parent__ = None
-
-    def getId(self):
-        return self.__name__ or ''
 
 
 class MacroStartConditionAdapter(FactoryAdapter):
     factory = MacroStartConditionObject
 
 
-class MacroEndConditionObject(object):
+class MacroEndConditionObject(BaseConditionObject):
     implements(IMacroEndConditionSchema)
 
     condition = FieldProperty(IMacroEndConditionSchema['condition'])
     operator = FieldProperty(IMacroEndConditionSchema['operator'])
-
-    __name__ = ''
-    __parent__ = None
-
-    def getId(self):
-        return self.__name__ or ''
 
 
 class MacroEndConditionAdapter(FactoryAdapter):
