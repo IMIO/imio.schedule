6a391e6adef0d516966de64c1c6fa6033061a361
Simon Delcourt <delcourt.simon@gmail.com>
2016-07-06 15:55:19 +0200

    Bring robot tests back to life.

--- src/imio/schedule/testing.py
+++ src/imio/schedule/testing.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from plone import api
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
@@ -186,6 +187,9 @@ class MacrotaskScheduleLayer(ScheduleLayer):
     def setUpPloneSite(self, portal):
         super(MacrotaskScheduleLayer, self).setUpPloneSite(portal)
 
+        applyProfile(portal, 'Products.CMFPlone:dependencies')
+        # ponr skin for robot tests
+        applyProfile(portal, 'plonetheme.sunburst:default')
         applyProfile(portal, 'imio.schedule:testing')
 
         # delete simple tasks
@@ -259,3 +263,13 @@ class MacroTaskScheduleFunctionalTestCase(MacroTaskScheduleTestBase):
         transaction.commit()
 
         super(MacroTaskScheduleTestBase, self).tearDown()
+
+
+SCHEDULE_TEST_ROBOT = FunctionalTesting(
+    bases=(
+        MACROTASK_SCHEDULE_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE
+    ),
+    name="URBAN_ROBOT"
+)
