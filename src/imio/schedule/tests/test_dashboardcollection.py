c7b497a949ff9190b0b5044c8c2d2830dbcc264f
Simon Delcourt <delcourt.simon@gmail.com>
2017-01-19 13:29:25 +0100

    Fix tests.

--- src/imio/schedule/tests/test_dashboardcollection.py
+++ src/imio/schedule/tests/test_dashboardcollection.py
@@ -4,6 +4,9 @@ from imio.schedule.testing import ExampleScheduleFunctionalTestCase
 
 from plone import api
 
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
+
 
 class TestDashboardCollectionFunctional(ExampleScheduleFunctionalTestCase):
     """
@@ -15,13 +18,13 @@ class TestDashboardCollectionFunctional(ExampleScheduleFunctionalTestCase):
         """
         task_config = self.task_config
         collection = self.task_config.dashboard_collection
-        self.assertEquals(task_config.title, dashboard_collection.title)
+        self.assertEquals(task_config.title, collection.title)
 
         task_config.title = 'my new title'
         notify(ObjectModifiedEvent(task_config))
-        self.assertEquals(task_config.title, dashboard_collection.title)
+        self.assertEquals(task_config.title, collection.title)
 
         msg = 'collection title should be reindexed when updated'
         catalog = api.portal.get_tool('portal_catalog')
-        brains = catalog(portal_type='DashboardCollection', title=task_config.title)
+        brains = catalog(portal_type='DashboardCollection', Title=task_config.title)
         self.assertEquals(len(brains), 1, msg)
