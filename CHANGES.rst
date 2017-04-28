a3a501aaa143c51a58f3873af0bcdc0b5404acf1
Simon Delcourt <delcourt.simon@gmail.com>
2017-04-28 09:54:34 +0200

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2017-04-28)
+----------------
 
 - Initial release.
   [sdelcourt]
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.schedule',
-    version='1.0a1',
+    version='1.0',
     description="Schedule for imio products",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
