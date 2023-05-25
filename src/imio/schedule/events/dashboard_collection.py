cb5a170d7bfcc73486d226aea7ec4766db435f2d
Martin Peeters <martin.peeters@affinitic.be>
2023-05-25 15:14:26 +0200

    Avoid an issue where title was incorrectly updated during migration

--- src/imio/schedule/events/dashboard_collection.py
+++ src/imio/schedule/events/dashboard_collection.py
@@ -72,6 +72,10 @@ def update_title(schedule_container, event):
     """
     Dashboard Collection title should always be the title of the parent task.
     """
+    # do not automatically re-create the collection during upgrade steps
+    if 'portal_setup' in schedule_container.REQUEST.URL:
+        return
+
     collection = getattr(schedule_container, 'dashboard_collection', None)
     if collection:
         title = IScheduleConfig.providedBy(schedule_container) and _('All') or schedule_container.Title()
