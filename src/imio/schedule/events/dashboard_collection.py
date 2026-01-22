f2ffe762900fc6534dc1d73496fdaa35044618b2
Julien Chandelle <julienchandelle@gmail.com>
2026-01-22 12:44:16 +0100

    URBBDC-3205 Move create dashboard collection function in utils (#17)
    * URBBDC-3205 Add way to force dashboard creation
    * URBBDC-3205 Change dashboard collection function

--- src/imio/schedule/events/dashboard_collection.py
+++ src/imio/schedule/events/dashboard_collection.py
@@ -2,11 +2,7 @@
 
 from imio.schedule import _
 from imio.schedule.content.schedule_config import IScheduleConfig
-from imio.schedule.interfaces import IScheduleCollection
-
-from plone import api
-
-from zope.interface import alsoProvides
+from imio.schedule.utils import create_dashboard_collection
 
 
 def create(schedule_container, event):
@@ -18,56 +14,7 @@ def create(schedule_container, event):
     ):
         return
 
-    collection_id = "dashboard_collection"
-    title = (
-        IScheduleConfig.providedBy(schedule_container)
-        and _("All")
-        or schedule_container.Title()
-    )
-
-    if collection_id not in schedule_container.objectIds():
-        factory_args = {
-            "id": "dashboard_collection",
-            "title": title,
-            "query": [
-                {
-                    "i": "CompoundCriterion",
-                    "o": "plone.app.querystring.operation.compound.is",
-                    "v": schedule_container.UID(),
-                },
-                {
-                    "i": "review_state",
-                    "o": "plone.app.querystring.operation.selection.is",
-                    "v": ["to_do"],
-                },
-            ],
-            "customViewFields": (u"assigned_user", u"status", u"due_date"),
-            "sort_on": u"due_date",
-            "sort_reversed": True,
-            "b_size": 30,
-        }
-
-        if IScheduleConfig.providedBy(schedule_container):
-            factory_args["customViewFields"] = (
-                u"pretty_link",
-                u"sortable_title",
-                u"assigned_user",
-                u"status",
-                u"due_date",
-            )
-
-        kwargs = {}
-        additional_queries = kwargs.pop("query", [])
-        for query in additional_queries:
-            factory_args["query"].append(query)
-        factory_args.update(kwargs)
-
-        portal_types = api.portal.get_tool("portal_types")
-        type_info = portal_types.getTypeInfo("DashboardCollection")
-        collection = type_info._constructInstance(schedule_container, **factory_args)
-        # mark the collection with an interface to to customize the render
-        # term view of collection widget
-        alsoProvides(collection, IScheduleCollection)
+    create_dashboard_collection(schedule_container)
 
 
 def update_title(schedule_container, event):
